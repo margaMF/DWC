5e07833036a0b655640d99748f9c83ea82b57f6a
margaMF <mmoyafiol@gmail.com>
2022-10-21 16:13:41 +0200

    Función App

---
+++ memorin/js/memorin.js
@@ -0,0 +1,9 @@
+function App() {
+
+    //Pedimos el maxFilas y maxColumnas al usuario.
+    let maxFilas = prompt('¿Cuántas filas quieres?');
+    let maxColumnas = prompt('¿Cuantas columnas quieres?');
+
+}
+
+App();
