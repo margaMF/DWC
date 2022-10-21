050717af60fc26aed0ca6630dee8864c059f7409
margaMF <mmoyafiol@gmail.com>
2022-10-21 17:05:57 +0200

    Primero funciones, luego lo pasaré a clases

--- memorin/js/memorin.js
+++ memorin/js/memorin.js
@@ -4,6 +4,20 @@ function App() {
     let maxFilas = prompt('¿Cuántas filas quieres?');
     let maxColumnas = prompt('¿Cuantas columnas quieres?');
 
+
+    
+    if ((maxFilas % 2) != 0 && (maxColumnas % 2) != 0) {
+
+            let maxFilas = prompt('Error!! La cantidad de filas y columnas debe ser un número par ¿Cuántas filas quieres?');
+            let maxColumnas = prompt('¿Cuantas columnas quieres?');
+        
+
+    }
+
+   
+
+
 }
+
 
 App();
