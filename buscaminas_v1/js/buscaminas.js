228b320ae2830aa7dd5cdd147e4c288efb012c87
margaMF <mmoyafiol@gmail.com>
2022-10-24 17:42:42 +0200

    correccion funcion App

--- buscaminas_v1/js/buscaminas.js
+++ buscaminas_v1/js/buscaminas.js
@@ -89,3 +89,4 @@ function AppFuncionando() {
     }
     
     AppFuncionando();
+    console.log(crearArrayTablero);
--- memorin/js/memorin.js
+++ memorin/js/memorin.js
@@ -3,21 +3,30 @@ function App() {
     //Pedimos el maxFilas y maxColumnas al usuario.
     let maxFilas = prompt('¿Cuántas filas quieres?');
     let maxColumnas = prompt('¿Cuantas columnas quieres?');
-
+    
+    totalCasillas = maxFilas * maxColumnas;
 
     
-    if ((maxFilas % 2) != 0 && (maxColumnas % 2) != 0) {
-
-            let maxFilas = prompt('Error!! La cantidad de filas y columnas debe ser un número par ¿Cuántas filas quieres?');
-            let maxColumnas = prompt('¿Cuantas columnas quieres?');
+    if ((totalCasillas % 2) != 0) {
+        let esPar = false;
         
+            while(esPar == false){
+                
+                let maxFilas = prompt('¡¡¡ERROR!!! La cantidad de filas y columnas debe ser un número par ¿Cuántas filas quieres?');
+                let maxColumnas = prompt('¿Cuantas columnas quieres?');
 
-    }
+                let totalCasillas = maxFilas * maxColumnas;
 
-   
+                if ((totalCasillas % 2) == 0){
+                    esPar == true;
+                    break;
+                } 
+                
+            }
+        
+    } 
 
 
 }
-
 
 App();
