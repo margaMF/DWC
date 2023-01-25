e533ceaae4570efe2dddc48459d7e1545b5e823a
margaMF <mmoyafiol@gmail.com>
2023-01-25 18:41:42 +0100

    Formulario

---
+++ validacion_formulario_v1/js/fromulario.js
@@ -0,0 +1,29 @@
+//Función para validar el nombre y apellidos del formulario
+function validarNombre(){
+    let patron = /^[A-Za-záéíóúüàèiòÁÉÍÓÚÀÈÒÜñÑçÇ]{2,}$/;
+
+    this.className = "";
+    if (patron.test(this.value)) {
+        this.className = "verde";
+    }
+}
+
+//Función para validar el email del formulario
+function validarEmail(){
+    let patron = /^.+@.+$/;
+
+    this.className = "";
+    if (patron.test(this.value)) {
+        this.className = "verde";
+    }
+}
+
+window.addEventListener('load', function(){
+    let nombre = document.getElementById('nombre');
+    let apellidos = document.getElementById('apellidos');
+    let email = document.getElementById('email');
+
+    nombre.addEventListener('keyup', validarNombre);
+    apellidos.addEventListener('keyup', validarNombre);
+    email.addEventListener('keyup', validarEmail);
+});
